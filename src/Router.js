5e1e72da13e52b23e36ae3ce408cf070477c4e58
최현민 <hymn9613@gmail.com>
2023-04-15 13:48:44 +0900

    feat(Router.js): 회원가입, 로그인, TODO 경로 설정

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Signup from './pages/Signup/Signup';
+import Signin from './pages/Signin/Signin';
+import Todo from './pages/Todo/Todo';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
