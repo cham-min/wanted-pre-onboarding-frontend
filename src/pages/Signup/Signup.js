02f2d6c36c3b59a415ba7b1d3f1fae986c84a260
최현민 <hymn9613@gmail.com>
2023-04-15 17:21:23 +0900

    feat: assignment1 유효성 검사 구현

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,7 +1,122 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import useInput from '../../hooks/useInput';
 
 const Signup = () => {
-  return <div>Signup Page</div>;
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+
+  const isValid = email.includes('@') && password.length >= 8;
+
+  return (
+    <SignupContainer>
+      <SignupWrapper>
+        <Logo>
+          <h1 style={{ fontSize: '25px' }}>Wanted TODO</h1>
+        </Logo>
+        <Form>
+          <FormDiv>
+            <label htmlFor="user-email">이메일</label>
+            <FormInput
+              name="user-email"
+              type="email"
+              value={email}
+              onChange={onChangeEmail}
+              placeholder="example@naver.com"
+              required
+            />
+          </FormDiv>
+          <FormDiv>
+            <label htmlFor="user-password">패스워드</label>
+            <FormInput
+              name="user-password"
+              type="password"
+              value={password}
+              onChange={onChangePassword}
+              placeholder="비밀번호 8자 이상"
+              required
+            />
+          </FormDiv>
+          <FormDiv>
+            <FormButton type="submit" disabled={!isValid}>
+              회원가입
+            </FormButton>
+          </FormDiv>
+        </Form>
+        <SignParagraph>
+          이미 회원인 경우
+          <a
+            href="/signin"
+            style={{
+              paddingLeft: '0.5rem',
+              fontWeight: 'bold',
+              color: '#658dc6',
+            }}
+          >
+            로그인
+          </a>
+        </SignParagraph>
+      </SignupWrapper>
+    </SignupContainer>
+  );
 };
+
+const SignupContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const SignupWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 22rem;
+  padding: 2rem;
+  border: 1px solid gray;
+  border-radius: 1rem;
+`;
+
+const Logo = styled.header`
+  display: flex;
+  justify-content: center;
+  padding: 1rem 0rem;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FormDiv = styled.div`
+  width: 100%;
+  padding: 0.7rem 0rem;
+`;
+
+const FormInput = styled.input`
+  width: 100%;
+  height: 2.2rem;
+  padding: 0.5rem;
+`;
+
+const FormButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 2.5rem;
+  border: 1px solid black;
+  background-color: #658dc6;
+  font-weight: bold;
+`;
+
+const SignParagraph = styled.p`
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+`;
 
 export default Signup;
