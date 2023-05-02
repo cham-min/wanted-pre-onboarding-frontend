d0c9078ce81e41956dfc6e0cb2d307fcf94ba1a3
Hyunmin Choi <hymn9613@gmail.com>
2023-05-02 20:34:48 +0900

    refactor: 구조분해할당

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -2,13 +2,13 @@ import customAxios from '../lib/customAxios';
 
 const authAPI = () => {
   const signup = async userInfo => {
-    const result = await customAxios.post('/auth/signup', userInfo);
-    return result.data;
+    const { data } = await customAxios.post('/auth/signup', userInfo);
+    return data;
   };
 
   const signin = async userInfo => {
-    const result = await customAxios.post('/auth/signin', userInfo);
-    return result.data;
+    const { data } = await customAxios.post('/auth/signin', userInfo);
+    return data;
   };
 
   return { signup, signin };
